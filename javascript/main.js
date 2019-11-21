ffa1a7aa40cbb509d6d26c956ac494e64da7b1ac
Øyvind Holtskog <oholtskog@hotmail.com>
2019-11-21 17:33:26 +0100

    Update main.js and index.html

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,7 @@ function aboutScroll(){
 }
 function topScroll(){
   var item = document.getElementById("top");
-  zenscroll.toY(0, 3000);
+  zenscroll.toY(0, 10000);
 }
 function imageScroll(){
   var item = document.getElementById("abt-me");

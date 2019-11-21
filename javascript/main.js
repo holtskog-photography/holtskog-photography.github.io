2b0c821d857a67126fdee72a57588785d9d185b9
Øyvind Holtskog <oholtskog@hotmail.com>
2019-11-21 16:41:52 +0100

    Add javascripts for smooth scroll

---
+++ javascript/main.js
@@ -0,0 +1,56 @@
+function contentScroll(){
+  var item = document.getElementById("self-top");
+  zenscroll.center(item, 2000, 200);
+}
+function cvScroll(){
+  var item = document.getElementById("cv");
+  zenscroll.center(item, 2000);
+}
+function aboutScroll(){
+  var item = document.getElementById("abt-me");
+  zenscroll.center(item, 3000);
+}
+function topScroll(){
+  var item = document.getElementById("top");
+  zenscroll.toY(0, 3000);
+}
+function imageScroll(){
+  var item = document.getElementById("abt-me");
+  zenscroll.center(item, 2000);
+}
+
+function loopDown(){
+       $("#to-top").animate({
+           marginTop : -10
+           },
+           500, function() {
+           loopUp();
+       });
+   }
+   function loopUp(){
+       $("#to-top").animate({
+           marginTop : 0
+           },
+           500, function() {
+           loopDown();
+       });
+   }
+ loopDown();
+
+function loopDwn(){
+       $("#footer").animate({
+           marginTop : 0
+           },
+           500, function() {
+           loopup();
+       });
+   }
+   function loopup(){
+       $("#footer").animate({
+           marginTop : -10
+           },
+           500, function() {
+           loopDwn();
+       });
+   }
+loopDwn();
